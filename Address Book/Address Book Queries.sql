37b84ad394b918d88ddb4969efe20ab1c522cb85
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-10-31 10:08:47 +0530

    SQL Queries Added

---
+++ Address Book/Address Book Queries.sql
@@ -0,0 +1,69 @@
+/*UC1*/
+create database AddressBookDB
+
+/*UC2*/
+create table AddressBook(
+id int NOT NULL identity(1,1) PRIMARY KEY,
+fName varchar(30) NOT NULL,
+lName varchar(30) NOT NULL,
+addr varchar(250) NOT NULL,
+city varchar(250) NOT NULL,
+state varchar(250) NOT NULL,
+phNo varchar(250) NOT NULL,
+email varchar(250) NOT NULL,
+);
+select * from AddressBook
+
+/*UC3*/
+insert into AddressBook(fName,lName,addr,city,state,phNo,email)
+values('Aru','Roy','Pimpri','Pune','Mah',776578976,'dxfc@tfg.dh'),
+('Neha','Roy','Worli','Mumbai','Mah',9857836878,'dchbhn@hjn.bhj'),
+('Shreya','Roy','Colaba','Mumbai','Mah',8765778676,'ugb@hgb.guvhjb'),
+('Bec','John','Kormangla','Bangalore','Karnataka',9897867565,'usz@bh.hbjk')
+select * from AddressBook
+
+/*UC4*/
+UPDATE AddressBook set lName='ROY'
+where fName='Aru'
+select * from AddressBook
+
+/*UC5*/
+delete from AddressBook
+where fName='Aru' and id=1
+select * from AddressBook
+
+/*UC6*/
+select * from AddressBook
+where state='Mah'
+
+/*UC7*/
+select count(*) from AddressBook
+where state='Mah'
+
+/*UC8*/
+select * from AddressBook
+where state='Mah'
+order by fName
+
+/*UC9*/
+alter table AddressBook
+add name varchar(30), type varchar(30)
+select * from AddressBook
+
+/*UC10*/
+select count(*) from AddressBook
+where type='Family'
+
+/*UC11*/
+insert into AddressBook(fName,lName,addr,city,state,phNo,email,name,type)
+values('Arundhati','Roy','Pimpri','Pune','Mah',776578976,'dxfc@tfg.dh','a','Family'),
+('Neharika','Roy','Worli','Mumbai','Mah',9857836878,'dchbhn@hjn.bhj','a','Friend'),
+('Shreyasi','Roy','Colaba','Mumbai','Mah',8765778676,'ugb@hgb.guvhjb','b','Family'),
+('Becty','John','Kormangla','Bangalore','Karnataka',9897867565,'usz@bh.hbjk','b','Friend')
+select * from AddressBook
+
+UPDATE AddressBook set type='Family',name='b'
+where fName='Aru'
+select * from AddressBook
+
+
