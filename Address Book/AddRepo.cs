a8809b1c65a05d771f787811968c204cdb5a6bf3
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-11-06 14:44:35 +0530

    Threads added to Address Book

--- Address Book/AddRepo.cs
+++ Address Book/AddRepo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Data.SqlClient;
 using System.Data;
+using System.Threading.Tasks;
 //using AddressBookProblem.Contacts;
 
 namespace AddressBookProblem
@@ -14,14 +15,14 @@ namespace AddressBookProblem
             return new SqlConnection(@"Data Source=(LocalDb)\RoyDB;Initial Catalog=AddressBookDB;Integrated Security=True");
         }
 
-        public string UpdateAddBook(string type,string name)
+        public string UpdateAddBook(string type, string name)
         {
+            var retType = "";
             SqlConnection sqlConnection = ConnSetup();
             //int salary = 0;
             try
             {
                 Contact contact = new Contact();
-                //var type = "";
                 using (sqlConnection)
                 {
                     sqlConnection.Open();
@@ -64,6 +65,8 @@ namespace AddressBookProblem
                             var email = dr.GetString(8);
                             var adName = dr.GetString(9);
                             type = dr.GetString(10);
+                            if (adName == name)
+                                retType = type;
 
                             Console.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}",
                                 contactId, contact.fName, contact.lName, contact.pNumber, contact.address, contact.city, contact.state, email,
@@ -71,7 +74,7 @@ namespace AddressBookProblem
                             Console.WriteLine("\n");
                         }
 
-                        
+
                     }
                     else
                     {
@@ -79,7 +82,6 @@ namespace AddressBookProblem
                     }
                     dr.Close();
                     sqlConnection.Close();
-                    return type;
                 }
             }
             catch (Exception e)
@@ -91,9 +93,184 @@ namespace AddressBookProblem
             {
                 sqlConnection.Close();
             }
-            //return salary;
+            return retType;
         }
 
+        public int DelAddressBookType()
+        {
+            SqlConnection sqlConnection = ConnSetup();
+            try
+            {
+                using (sqlConnection)
+                {
+                    string query = @"delete from AddressBookDictionary where name='c'";
+
+                    //define sql object
+                    SqlCommand cmd = new SqlCommand(query, sqlConnection);
+
+                    sqlConnection.Open();
+
+                    //cmd.Parameters.AddWithValue("@name", "nallia");
+
+                    int rows = cmd.ExecuteNonQuery();
+                    if (rows > 0)
+                    {
+                        Console.WriteLine(rows + " row(s) affected");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please check your query");
+                    }
+                    sqlConnection.Close();
+                    return rows;
+                }
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine("Null data found");
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+
+
+        public void AddToContact_CheckTypeFromAddressDict(string fname, string lname, string phNo, string type)
+        {
+            SqlConnection sqlConnection = ConnSetup();
+            try
+            {
+                Contact contact = new Contact();
+                using (sqlConnection)
+                {
+                    sqlConnection.Open();
+
+                    //define sql object
+                    SqlCommand cmd = new SqlCommand("spAddContact", sqlConnection);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@type", type);
+                    cmd.Parameters.AddWithValue("@fname", fname);
+                    cmd.Parameters.AddWithValue("@lname", lname);
+                    cmd.Parameters.AddWithValue("@phNo", phNo);
+
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if (dr.HasRows)
+                    {
+                        while (dr.Read())
+                        {
+                            var contactId = dr.GetInt32(0);
+                            contact.fName = dr.GetString(2);
+                            contact.lName = dr.GetString(3);
+                            contact.pNumber = dr.GetString(4);
+                            if (dr["addr"] != DBNull.Value)
+                                contact.address = dr.GetString(5);
+                            if (dr["city"] != DBNull.Value)
+                                contact.city = dr.GetString(6);
+                            if (dr["state"] != DBNull.Value)
+                                contact.state = dr.GetString(7);
+                            var email = dr.GetString(8);
+                            var adName = dr.GetString(9);
+                            type = dr.GetString(10);
+
+                            Console.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}",
+                                contactId, contact.fName, contact.lName, contact.pNumber, contact.address, contact.city, contact.state, email,
+                                adName, type);
+                            Console.WriteLine("\n");
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("No data found");
+                    }
+
+                    dr.Close();
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine("Null data found");
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        public void AddToContact_CheckTypeFromAddressDictWithThread(string fname, string lname, string phNo, string type)
+        {
+            Task thread = new Task(() =>
+            {
+                SqlConnection sqlConnection = ConnSetup();
+                try
+                {
+                    Contact contact = new Contact();
+                    using (sqlConnection)
+                    {
+                        sqlConnection.Open();
+
+                        //define sql object
+                        SqlCommand cmd = new SqlCommand("spAddContact", sqlConnection);
+
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@type", type);
+                        cmd.Parameters.AddWithValue("@fname", fname);
+                        cmd.Parameters.AddWithValue("@lname", lname);
+                        cmd.Parameters.AddWithValue("@phNo", phNo);
+
+                        SqlDataReader dr = cmd.ExecuteReader();
+
+                        if (dr.HasRows)
+                        {
+                            while (dr.Read())
+                            {
+                                var contactId = dr.GetInt32(0);
+                                contact.fName = dr.GetString(2);
+                                contact.lName = dr.GetString(3);
+                                contact.pNumber = dr.GetString(4);
+                                if (dr["addr"] != DBNull.Value)
+                                    contact.address = dr.GetString(5);
+                                if (dr["city"] != DBNull.Value)
+                                    contact.city = dr.GetString(6);
+                                if (dr["state"] != DBNull.Value)
+                                    contact.state = dr.GetString(7);
+                                var email = dr.GetString(8);
+                                var adName = dr.GetString(9);
+                                type = dr.GetString(10);
+
+                                Console.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}",
+                                    contactId, contact.fName, contact.lName, contact.pNumber, contact.address, contact.city, contact.state, email,
+                                    adName, type);
+                                Console.WriteLine("\n");
+                            }
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("No data found");
+                        }
+
+                        dr.Close();
+                        sqlConnection.Close();
+                    }
+                }
+                catch (Exception e)
+                {
+                    //Console.WriteLine("Null data found");
+                    throw new Exception(e.Message);
+                }
+                finally
+                {
+                    sqlConnection.Close();
+                }
+            });
+            thread.Start();
+        }
 
     }
 }
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,23 +1,70 @@
 using AddressBookProblem;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace UnitTestProject1
 {
     [TestClass]
     public class UnitTest1
     {
+        /// <summary>
+        /// Updates in address book should reflect in contact.
+        /// </summary>
         [TestMethod]
         public void UpdateInAddressBook_ChangeInContact()
         {
             //Arrange
             AddRepo addRepo = new AddRepo();
-            Contact payroll = new Contact();
 
             //Act
             string type = addRepo.UpdateAddBook("Acquaintance","a");
 
             //Arrange
             Assert.AreEqual("Acquaintance",type);
+        }
+
+        /// <summary>
+        /// Adds the mult employee record time.
+        /// </summary>
+        [TestMethod]
+        public void AddMultEmployee_RecordTime()
+        {
+            //Arrange
+            AddRepo addRepo = new AddRepo();
+
+            //Act
+            DateTime sd = DateTime.Now;
+            addRepo.AddToContact_CheckTypeFromAddressDict("Abhimanyu","Roy","635876987","Colleagues");
+            addRepo.AddToContact_CheckTypeFromAddressDict("Samay", "Raina", "84776987", "Friend");
+            addRepo.AddToContact_CheckTypeFromAddressDict("Neha", "Roy", "9845976987", "Family");
+            addRepo.AddToContact_CheckTypeFromAddressDict("Jeet", "Roy", "635876987", "Acquaintance");
+            DateTime ed = DateTime.Now;
+
+            //Arrange
+            Console.WriteLine("Duration {0}", sd - ed);
+        }
+
+        /// <summary>
+        /// Adds the mult employee record time withthread.
+        /// </summary>
+        [TestMethod]
+        public void AddMultEmployee_RecordTimeWiththread()
+        {
+            //Arrange
+            AddRepo addRepo = new AddRepo();
+            //addRepo.DelAddressBookType();
+
+            //Act
+            DateTime sd = DateTime.Now;
+            addRepo.AddToContact_CheckTypeFromAddressDictWithThread("Abhimanyu", "Roy", "635876987", "Family");
+            addRepo.AddToContact_CheckTypeFromAddressDictWithThread("Samay", "Roy", "84776987", "Friend");
+            addRepo.AddToContact_CheckTypeFromAddressDictWithThread("Neha", "Roy", "9845976987", "Family");
+            addRepo.AddToContact_CheckTypeFromAddressDictWithThread("Jeet", "Roy", "635876987", "Acquaintance");
+            DateTime ed = DateTime.Now;
+
+            //Arrange
+            //Assert.AreEqual(payroll.basicPay, sal);
+            Console.WriteLine("Duration {0}", sd - ed);
         }
     }
 }
